01a6506b6326240306a766f2063d57f1bdb96514
perikoloko <pedroesposito2015@gmail.com>
2023-12-18 14:01:06 +0100

    Mejorada legibilidad del código

--- AlumnoCRUD.java
+++ AlumnoCRUD.java
@@ -66,11 +66,9 @@ escribirAlumnos(listaAlumno);
 }
 public void bajaAlumno(String dni) {
 ArrayList<Alumno> listaAlumno = leerAlumnos();
-for (int i = 0; i < listaAlumno.size(); i++) {
-if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+for (int i = 0; i < listaAlumno.size(); i++)
+if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni))
 listaAlumno.remove(i);
-}
-}
 escribirAlumnos(listaAlumno);
 }
 public void actualizarAlumno(Alumno alumno) {
