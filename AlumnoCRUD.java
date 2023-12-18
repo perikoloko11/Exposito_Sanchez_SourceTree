87650a7a0c119defa128e3634b0fdb5c2f8e4995
perikoloko <pedroesposito2015@gmail.com>
2023-12-18 13:51:32 +0100

    Añadido método altaAlumno

--- AlumnoCRUD.java
+++ AlumnoCRUD.java
@@ -58,3 +58,9 @@ public class AlumnoCRUD {
 
 
 }
+
+public void altaAlumno(Alumno alumno) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+listaAlumno.add(alumno);
+escribirAlumnos(listaAlumno);
+}
