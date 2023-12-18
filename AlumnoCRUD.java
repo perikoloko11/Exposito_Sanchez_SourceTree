d6eebe7dfd3264dde0723bf030e9f49da2229a57
perikoloko <pedroesposito2015@gmail.com>
2023-12-18 13:57:05 +0100

    Añadido método actualizarAlumno

--- AlumnoCRUD.java
+++ AlumnoCRUD.java
@@ -73,3 +73,13 @@ listaAlumno.remove(i);
 }
 escribirAlumnos(listaAlumno);
 }
+public void actualizarAlumno(Alumno alumno) {
+ArrayList<Alumno> listaAlumnos = leerAlumnos();
+for (int i = 0; i < listaAlumnos.size(); i++) {
+if (listaAlumnos.get(i).getDni()
+.equalsIgnoreCase(alumno.getDni())) {
+listaAlumnos.set(i, alumno);
+}
+}
+escribirAlumnos(listaAlumnos);
+}
