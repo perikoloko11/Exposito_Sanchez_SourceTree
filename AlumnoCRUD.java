f859ba2e617c4fabee241b7de7ff6a6f47820d94
[alvaroosxnchzz] <[alvarosanchezortega34@gmail.com]>
2023-12-18 13:54:22 +0100

    Añadido método bajaAlumno

--- AlumnoCRUD.java
+++ AlumnoCRUD.java
@@ -64,3 +64,12 @@ ArrayList<Alumno> listaAlumno = leerAlumnos();
 listaAlumno.add(alumno);
 escribirAlumnos(listaAlumno);
 }
+public void bajaAlumno(String dni) {
+ArrayList<Alumno> listaAlumno = leerAlumnos();
+for (int i = 0; i < listaAlumno.size(); i++) {
+if (listaAlumno.get(i).getDni().equalsIgnoreCase(dni)) {
+listaAlumno.remove(i);
+}
+}
+escribirAlumnos(listaAlumno);
+}
